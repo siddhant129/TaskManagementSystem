d80a9fcacfb70dea290b18a49fbb49f273537e8f
siddhant halpatrao <siddhanthalpatrao@gmail.com>
2024-05-27 00:05:36 +0530

    Feat : Home page updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,7 @@
 import { LogInNav } from "./LogInNav";
 import tms from "../Images/tms.avif";
+import supimg from "../Images/signupimg.png";
+import teamcomm from "../Images/teamcomm.jpg";
 
 export function Home() {
   return (
@@ -10,14 +12,22 @@ export function Home() {
         <div className="grid grid-cols-2 gap-6 bg-homeBg scroll-smooth">
           <div className="">
             <h1 className="text-3xl ml-2 mt-20 transition delay-300 duration-600 ease-in-out text-center font-bold transition-all transform">
-              Welcome to our Task Management System
+              <span className="text-cyan-500"> Welcome </span>to our Task{" "}
+              <span className="text-orange-300"> Management</span> System
             </h1>
-            <div className="flex justify-center mt-40">
+            <p className="text-center m-20 text-xl  justify-items-center">
+              <span>Start managing your tasks from anywhere </span>
+              <span>
+                {" "}
+                And create your teams and communicate with your team members
+              </span>
+            </p>
+            <div className="flex justify-center mt-30">
               {/* About button */}
               <a
                 href="#About"
                 type="button"
-                class="select-none m-2 rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                class="select-none m-2 rounded-lg bg-gray-900 hover:text-orange-300 hover:bg-cyan-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               >
                 About
               </a>
@@ -25,7 +35,7 @@ export function Home() {
               <a
                 href="/signUp"
                 type="button"
-                className="select-none m-2 rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                className="select-none m-2 hover:text-cyan-500 hover:bg-orange-300 rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               >
                 Get started
               </a>
@@ -39,13 +49,24 @@ export function Home() {
 
         <div id="About" className="justify-between scroll-smooth">
           <div className=" h-full transition delay-150 duration-300 ease-in-out  m-2 rounded-md ring-1 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-250 dark:bg-slate-100 dark:highlight-white/5 dark:hover:bg-slate-200">
-            <p className="text-center text-2xl mt-20">
-              Just sign up to start managing your tasks
-            </p>
-            <p className="text-left text-xl pt-40">
-              Create you tasks folder and build your team to communicate with
-              them
-            </p>
+            <h1 className="text-center text-3xl font-bold text-sky-400">
+              About
+            </h1>
+            <div className="grid grid-cols-2">
+              <img src={supimg} alt="" />
+              <p className="text-center text-2xl m-10 mt-20">
+                To effectively manage your tasks, sign up now and begin creating
+                them with proper organization and management techniques.{" "}
+              </p>
+            </div>
+            <div className="grid grid-cols-2">
+              <p className="text-center text-xl m-10 mt-20">
+                Establish your own chat-based team to enhance communication and
+                effectively track task statuses, fostering efficient
+                collaboration and productivity.
+              </p>
+              <img className="rounded-full w-9/12" src={teamcomm} alt="" />
+            </div>
           </div>
         </div>
       </div>
--- src/Components/LogInNav.jsx
+++ src/Components/LogInNav.jsx
@@ -23,7 +23,7 @@ export function LogInNav() {
 
   return (
     <>
-      <nav className="bg-gray-800 sticky top-0">
+      <nav className="bg-gray-800 sticky top-0 scroll-smooth">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -100,14 +100,14 @@ export function LogInNav() {
                 <div className="flex space-x-4">
                   {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                   <a
-                    href="#"
+                    href="/#"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
                     aria-current="page"
                   >
                     Home
                   </a>
                   <a
-                    href="#"
+                    href="#About"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     About
@@ -180,7 +180,7 @@ export function LogInNav() {
           <div className={`space-y-1 px-2 pb-3 pt-2 transition-all ${menu}`}>
             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
             <a
-              href="#"
+              href="/"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               aria-current="page"
             >
